c3400b225ff11122cc09e4325cef4b7b4d9b3d09
Roman Naumenko <naumenko_subscr@mail.ru>
2020-03-16 00:03:59 +1000

    STEP-1 Added basic bundle mockup

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
+    bravik\CalendarBundle\CalendarBundle::class => ['all' => true],
 ];
